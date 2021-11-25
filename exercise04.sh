ca29cf62d30ccd04dbb504965031475f2a34a2f1
Foxglovve <92249714+Foxglovve@users.noreply.github.com>
2021-11-25 15:23:31 +0100

    Create exercise04.sh
    Commit für Aufgabe 1: Lange Version mit allen .csv Dateien einzeln und kurze Version mit * wildcard

---
+++ exercise04.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+$ grep -w -E '(kappa|trihard)' 1.csv | wc -l
+$ grep -w -E '(kappa|trihard)' 2.csv | wc -l
+$ grep -w -E '(kappa|trihard)' 3.csv | wc -l
+$ grep -w -E '(kappa|trihard)' 4.csv | wc -l
+$ grep -w -E '(kappa|trihard)' 5.csv | wc -l
+$ grep -w -E '(kappa|trihard)' 6.csv | wc -l
+$ grep -w -E '(kappa|trihard)' 7.csv | wc -l
+$ grep -w -E '(kappa|trihard)' 8.csv | wc -l
+$ grep -w -E '(kappa|trihard)' 9.csv | wc -l
+$ grep -w -E '(kappa|trihard)' 10.csv | wc -l
+$ grep -w -E '(kappa|trihard)' 11.csv | wc -l
+$ grep -w -E '(kappa|trihard)' 12.csv | wc -l
+$ grep -w -E '(kappa|trihard)' 13.csv | wc -l
+$ grep -w -E '(kappa|trihard)' *.csv | wc -l
