c62c2c621e38988713e1def5f4f6571ae9e9e871
Foxglovve <92249714+Foxglovve@users.noreply.github.com>
2021-11-25 16:08:42 +0100

    Update exercise04.sh
    Commit für Aufgabe 3: Chat Messages. Ist glaube ich nicht ganz richtig.

--- exercise04.sh
+++ exercise04.sh
@@ -15,3 +15,5 @@ $ grep -w -E '(kappa|trihard)' 13.csv | wc -l
 $ grep -w -E '(kappa|trihard)' *.csv | wc -l
 $ cut -d, -f2 *.csv > user_list.txt
 $ grep -E '(911|110)' user_list.txt | wc -l
+$ cut -d, -f3 *.csv > chatmessages.txt
+$ grep -E '.{20}' chatmessages.txt | wc -l
