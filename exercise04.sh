502e7cbbb921b7c8223eb39ab7001b73da8fdd1c
Foxglovve <92249714+Foxglovve@users.noreply.github.com>
2021-11-25 15:36:37 +0100

    Update exercise04.sh
    Zeilen 16 und 17 hinzugefügt für Shell Befehle zu Aufgabe 4.2 (911|110)

--- exercise04.sh
+++ exercise04.sh
@@ -13,3 +13,5 @@ $ grep -w -E '(kappa|trihard)' 11.csv | wc -l
 $ grep -w -E '(kappa|trihard)' 12.csv | wc -l
 $ grep -w -E '(kappa|trihard)' 13.csv | wc -l
 $ grep -w -E '(kappa|trihard)' *.csv | wc -l
+$ cut -d, -f2 *.csv > user_list.txt
+$ grep -E '(911|110)' user_list.txt | wc -l
